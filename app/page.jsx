1547e437d3ba958094ff6917eb0c0ae77b8b0731
megbug <meghub@proton.me>
2023-11-15 10:41:16 +0100

    changed content of Hompage

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,13 @@
-import Feed from '@components/Feed';
+import Link from 'next/link';
 
 const Home = () => {
-  return (
-    <section className='w-full flex-center flex-col'>
-        <h1 className='head_text text-center'>
-            Discover and Share<br className='max-md:hidden'/>
-            <span className='green_gradient text-center'>What you know</span>
-        </h1>
-        <p className="desc text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus tempore iure ipsa repellat, ratione tenetur placeat nemo non voluptatibus aut.</p>
-        <Feed />
-    </section>
-  )
+    return (
+        <main>
+            <h1>NextAuth.js</h1>
+            <Link href='/register'>Register Page</Link>
+            <Link href='/login'>Login Page</Link>
+        </main>
+    )
 }
 
 export default Home
