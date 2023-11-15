8c56bd1c6ec6fd1f89deb3efa0cd3ad78482d906
megbug <meghub@proton.me>
2023-11-15 10:32:45 +0100

    added provider, session, secret to authOptions

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,38 +9,51 @@ const prisma = new PrismaClient();
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma),
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import NextAuth from 'next-auth';
-// import GoogleProvider from 'next-auth/providers/google';
-/*
-const handler = NextAuth({
     providers: [
-        GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+                password: { label: 'Password', type: 'password' },
+                email: { label: 'Email', type: 'email' }
+            },
+            async authorize(credentials) {
+                // check if email and password is valid
+                if(!credentials.email || !credentials.password) {
+                    return null;
+                }
+
+                // check if user exists
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if(!user) {
+                    return null;
+                }
+                
+                // check if passwords match 
+                const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword);
+
+                if(!passwordMatch) {
+                    return null;
+                }
+
+                // return user if everything is correct
+
+                return user;
+            }
         })
     ],
-    async session({ session }) {
-
+    session: {
+        strategy: 'jwt',
     },
-    async signIn({ profile }) {
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === 'development',
+};
 
-    }
-})
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
-*/
+export { handler as GET, handler as POST }
